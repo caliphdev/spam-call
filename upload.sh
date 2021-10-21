7fc4a4766692667d3949e849d9901cd3f4fccd19
caliph91 <caliphatibrata71@gmail.com>
2021-10-21 15:52:30 +0700

    first commit

---
+++ upload.sh
@@ -0,0 +1,7 @@
+echo "# spam-call" >> README.md
+git init
+git add *
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/Caliph91/spam-call.git
+git push -u origin master
